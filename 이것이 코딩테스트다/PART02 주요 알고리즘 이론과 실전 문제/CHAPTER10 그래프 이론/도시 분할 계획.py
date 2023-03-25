64f823a709609b467e74316feb3406761c1d9da0
Leejiwon <dlwldnjs1009@gmil.com>
2023-03-25 22:50:01 +0900

    도시 분할 계획

--- 이것이 코딩테스트다/PART02 주요 알고리즘 이론과 실전 문제/CHAPTER10 그래프 이론/도시 분할 계획.py
+++ 이것이 코딩테스트다/PART02 주요 알고리즘 이론과 실전 문제/CHAPTER10 그래프 이론/도시 분할 계획.py
@@ -14,7 +14,7 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-# 노드의 개수와 간선(union 연산)의 개수 입력받기
+# 집의 개수와 간선(union 연산)의 개수 입력받기
 v, e = map(int, input().split())
 parent = [0] * (v + 1) # 부모 테이블 초기화
 
@@ -32,15 +32,14 @@ for _ in range(e):
     # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정 -> 파이썬은 기본적으로 최소 힙 구조 이용
     edges.append((cost, a, b))
 
-# 간선을 비용으로 정렬
 edges.sort()
+last = 0 # 최소 신장 트리에 포함되는 간선 중에서 가장 비용이 큰 간선
 
-# 간선을 하나씩 확인하며
 for edge in edges:
     cost, a, b = edge
-    # 사이클이 발생하지 않는 경우에만 집합에 포함
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
         result += cost
+        last = cost
 
-print(result)
+print(result - last)

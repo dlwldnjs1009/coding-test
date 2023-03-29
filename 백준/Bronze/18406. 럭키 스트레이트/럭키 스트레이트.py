70ba2b0d4001fad09cf06bd7f50dd95c68919c87
LeeJiWon <107186291+dlwldnjs1009@users.noreply.github.com>
2023-03-29 18:52:49 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
@@ -0,0 +1,12 @@
+data = input()
+
+n = len(data)
+half_n = n // 2
+
+sum_half_a = sum(map(int, data[:half_n]))
+sum_half_b = sum(map(int, data[half_n:]))
+
+if sum_half_a == sum_half_b:
+    print("LUCKY")
+else:
+    print("READY")

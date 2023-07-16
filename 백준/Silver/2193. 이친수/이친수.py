2f92e6862c078987dd37715f746b74f44e6a9434
LeeJiWon <107186291+dlwldnjs1009@users.noreply.github.com>
2023-07-16 16:41:25 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,10 +1,17 @@
-n = int(input())
+N = int(input())
 
-# n 자리 수 만큼 dp 테이블 생성 => n+1
-dp = [0] * (n + 1)
-dp[1] = 1
+dp = [[0, 0] for _ in range(N + 1)]
 
-for i in range(2, n+1):
-    dp[i] = dp[i-2] + dp[i-1]
 
-print(dp[n])
+dp[1][0] = 0
+dp[1][1] = 1
+
+for i in range(2, N + 1):
+    dp[i][0] = dp[i - 1][0] + dp[i - 1][1]
+    dp[i][1] = dp[i - 1][0]
+
+print(dp[N][0] + dp[N][1])
+
+
+
+

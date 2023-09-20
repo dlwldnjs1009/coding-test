fe120d1263965a2b67b1ce84a996053ca7d8c14f
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2023-09-20 17:24:31 +0900

    [Silver IV] Title: 수 찾기, Time: 276 ms, Memory: 141064 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,13 +1,23 @@
-# 딕셔너리는 O(1)이니 시간복잡도 면에서도 좋다
+# 이진 탐색(반복문)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) //2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
 n = int(input())
 A = list(map(int, input().split()))
-dict_A = {}
-for i in A:
-    dict_A[i] = True
 m = int(input())
 qustion_A = list(map(int, input().split()))
-for j in qustion_A:
-    if j in dict_A:
-        print(1)
-    else:
+sorted_A = sorted(A)
+
+for i in qustion_A:
+    if binary_search(sorted_A, i, 0, n-1) == None:
         print(0)
+    else:
+        print(1)

49569895189b8746f3e353aa35c713ca2875a69f
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2023-12-21 18:48:00 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,3 +1,17 @@
+def count_score(score):
+    if score == 6:
+        return 1
+    elif score == 5:
+        return 2
+    elif score == 4:
+        return 3
+    elif score == 3:
+        return 4
+    elif score == 2:
+        return 5
+    else:
+        return 6
+
 def solution(lottos, win_nums):
     answer = []
     exist = []
@@ -11,34 +25,8 @@ def solution(lottos, win_nums):
         else:
             zero += 1
     high = len(exist) + zero
-    if high == 6:
-        answer.append(1)
-    elif high == 5:
-        answer.append(2)
-    elif high == 4:
-        answer.append(3)
-    elif high == 3:
-        answer.append(4)
-    elif high == 2:
-        answer.append(5)
-    else:
-        answer.append(6)
+    answer.append(count_score(high))
     low = len(exist)
-    if low == 6:
-        answer.append(1)
-    elif low == 5:
-        answer.append(2)
-    elif low == 4:
-        answer.append(3)
-    elif low == 3:
-        answer.append(4)
-    elif low == 2:
-        answer.append(5)
-    else:
-        answer.append(6)
+    answer.append(count_score(low))
     return answer
-
-print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
-print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]	))
-print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
 

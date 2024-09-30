5126bacde8da36b62a298237f4cd921aad081dff
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2024-09-30 23:32:04 +0900

    Time: 39 ms (86.08%), Space: 21.9 MB (7.46%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 거꾸로 뒤집어도 똑같은 문장인지 판별
+        # 맨 중간 글자 빼고 나머지가 같은지 판별하면 되잖아
+        # 홀수는 (strs / 2) 짝수는 strs / 2 + 1 가 중간 글자
+        # len = 21 이면 0~20까지
+        # 이경우 중간글자 10임 리스트의 0~9, 11~20
+        # 0은 (len-1) -0 이랑 비교 1은 (len-1) - 1 이랑 비교 ~ 9는 (len-1) - 9랑 비교
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+         # 좌우를 비교하여 회문 판별
+        for i in range(len(strs) // 2):
+            if strs[i] != strs[len(strs) - 1 - i]:
+                return False
+        return True

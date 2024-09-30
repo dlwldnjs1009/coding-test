0ed7f624237817208341f3f615ad9b85b171fa38
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2024-09-30 23:35:46 +0900

    Time: 43 ms (68.53%), Space: 22 MB (5.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,17 +6,31 @@ class Solution:
         # len = 21 이면 0~20까지
         # 이경우 중간글자 10임 리스트의 0~9, 11~20
         # 0은 (len-1) -0 이랑 비교 1은 (len-1) - 1 이랑 비교 ~ 9는 (len-1) - 9랑 비교
-        strs = []
-        for char in s:
-            if char.isalnum():
-                strs.append(char.lower())
+        # strs = []
+        # for char in s:
+        #     if char.isalnum():
+        #         strs.append(char.lower())
 
          # 좌우를 비교하여 회문 판별
         # for i in range(len(strs) // 2):
         #     if strs[i] != strs[len(strs) - 1 - i]:
         #         return False
         # return True
+
+        # while len(strs) > 1:
+        #     if strs.pop(0) != strs.pop():
+        #         return False
+        # return True
+
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+        
         while len(strs) > 1:
-            if strs.pop(0) != strs.pop():
+            if strs.popleft() != strs.pop():
                 return False
+
+
         return True

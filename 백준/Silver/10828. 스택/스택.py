057b7e4b7820e116e6ff904b024a130f3ecaf85e
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-01-20 00:36:10 +0900

    [Silver IV] Title: 스택, Time: 424 ms, Memory: 113360 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n = int(input())
+# 반복문 돌면서 입력이랑 문장 if하면 되는거 아닌가
+stack = deque([])
+for i in range(n):
+    # 구분자로 잘라서 리스트에 저장 안의 문자에 따라 if로 나누기
+    op = input().split(' ')
+    if op[0] == 'push':
+        stack.appendleft(int(op[1]))
+    elif op[0] == 'top':
+        if len(stack) == 0 :
+            print(-1)
+        else:
+            print(stack[0])
+    elif op[0] == 'size':
+        print(len(stack))
+    elif op[0] == 'empty':
+        if len(stack) == 0 :
+            print(1)
+        else:
+            print(0)
+    elif op[0] == 'pop':
+        if len(stack) == 0 :
+            print(-1)
+        else:
+            print(stack.popleft())

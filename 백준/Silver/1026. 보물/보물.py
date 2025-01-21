6276c7342a163f8d957c3c8ef3ef5126ad339f96
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-01-21 15:09:28 +0900

    [Silver IV] Title: 보물, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,13 +1,16 @@
 n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
+# b의 최댓값들을 a의 최솟값과 곱해야 함.
+# 어떻게 곱할까?
+# b의 최댓값 인덱스를 알아야 함
+# a의 최솟값 인덱스
 
-temp_b = b
-a = sorted(a)
 
 sum = 0
 for i in range(n):
-    sum += (a[i] * temp_b[temp_b.index(max(temp_b))])
-    temp_b.remove(max(temp_b))
-
+    sum += min(a) * max(b)
+    a.pop(a.index(min(a)))
+    b.pop(b.index(max(b)))
 print(sum)
+

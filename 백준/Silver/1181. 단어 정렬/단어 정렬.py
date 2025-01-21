d4be174b35d5d5c69e70fd27a6e55cd203bf0e73
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-01-21 15:32:17 +0900

    [Silver V] Title: 단어 정렬, Time: 128 ms, Memory: 113832 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,31 +1,15 @@
+from sys import stdin
+
+words = []
 n = int(input())
-
-
-array = []
 for i in range(n):
-    a = input()
-    if a not in array:
-        array.append(a)
+    words.append(stdin.readline().rstrip())
 
+words_set = set(words)
+words = list(words_set)
 
-def quick_sort(array):
-    if len(array) < 1:
-        return array
+words.sort()
+words.sort(key=len)
 
-    pivot = array[0]
-    tail = array[1:]
-
-    left_side = []
-    right_side = []
-
-    for x in tail:
-        if len(x) < len(pivot) or (len(x) == len(pivot) and x < pivot):
-            left_side.append(x)
-        else:
-            right_side.append(x)
-
-    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
-
-result = quick_sort(array)
-for word in result:
-    print(word)
+for j in range(len(words)):
+    print(words[j])

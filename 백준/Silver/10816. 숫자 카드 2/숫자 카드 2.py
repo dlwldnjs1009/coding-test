8645d1f8829ad6337e0ecd8c27e0850757f0e100
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-01-23 01:55:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 488 ms, Memory: 263676 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,29 +1,17 @@
 import sys
+from collections import Counter
 
+input = sys.stdin.readline
 n = int(input())
-num_arr = list(map(int, sys.stdin.readline().split()))
+num_arr = list(map(int, input().split()))
 
-# 양수 음수 리스트 따로 만들면 되지 않을까
-positive_list = ([0] * 10000001)
-negative_list = ([0] * 10000001)
+card_counts = Counter(num_arr)
 
-for num in num_arr:
-    if num > 0:
-        positive_list[num] += 1
-    elif num == 0:
-        positive_list[0] += 1
-        negative_list[0] += 1
-    else:
-        num *= -1
-        negative_list[num] += 1
 m = int(input())
-ans_nums = list(map(int, sys.stdin.readline().split()))
-for ans_num in ans_nums:
-    if ans_num > 0:
-        print(positive_list[ans_num], end=' ')
-    elif ans_num == 0:
-        print((positive_list[0]), end=' ')
-    else:
-        ans_num *= -1
-        print(negative_list[ans_num], end=' ')
+query_nums = list(map(int, input().split()))
 
+result = []
+for num in query_nums:
+    result.append(str(card_counts[num]))
+
+print(' '.join(result))

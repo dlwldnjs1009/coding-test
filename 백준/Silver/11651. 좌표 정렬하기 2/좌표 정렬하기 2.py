40a03468683c79c9a076defe28ca9964a857c7d0
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-01-23 01:14:25 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 320 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -4,7 +4,7 @@ n = int(input())
 # sorted함수의 lambda 사용해서 첫번째y로 정렬 후 두번째x로 정렬 하도록
 a = []
 for _ in range(n):
-    b, c = map(int, input().split())
+    b, c = map(int, stdin.readline().split())
     a.append((b,c))
 
 a.sort(key= lambda x : (x[1], x[0]))

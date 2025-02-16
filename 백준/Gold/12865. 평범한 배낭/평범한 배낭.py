b5b5b98a9781b483b485d78b10071dedc3436682
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-02-16 23:04:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 332 ms, Memory: 189920 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,12 +1,19 @@
 n, k = map(int, input().split())
-dp = [[0] * (k + 1) for _ in range(n + 1)]
 
-for i in range(1, n + 1):
-    w, v = map(int, input().split())
-    for j in range(1, k + 1):
+knapsack = [[0,0]]
+d = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(n):
+    knapsack.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = knapsack[i][0]
+        v = knapsack[i][1]
+
         if j < w:
-            dp[i][j] = dp[i-1][j]
+            d[i][j] = d[i-1][j]
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
 
-print(dp[n][k])
+print(d[n][k])

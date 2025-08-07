52023b4787bab8b82ec64722c549ab784a4f7a2c
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-08-07 22:23:35 +0900

    [Silver I] Title: 2024는 무엇이 특별할까?, Time: 448 ms, Memory: 44012 KB -BaekjoonHub

---
+++ 백준/Silver/31247. 2024는 무엇이 특별할까？/2024는 무엇이 특별할까？.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long[] pow;
+    public static void main(String[] args) throws IOException {
+        pow = new long[64];
+        pow[0]= 1;
+        for (int i = 1; i <= 63; i++) {
+            pow[i] = pow[i-1] * 2;
+        }
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        StringBuilder result = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            long n = Long.parseLong(st.nextToken());
+            long k = Long.parseLong(st.nextToken());
+            
+            if (k >= 63) {
+                result.append(0).append("\n");
+            } else {
+                int tmp = (int) k;
+                long ans = (n / pow[tmp]);
+                // 여기 기억 안남
+                
+                if (ans % 2 == 0) {
+                    result.append(ans / 2).append("\n");
+                } else {
+                    result.append(ans - (ans - 1) / 2).append("\n");
+               }
+            }
+        }
+        System.out.println(result);
+    }
+}

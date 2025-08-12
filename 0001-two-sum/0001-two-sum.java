830ce984222f2096f28a32d3454d89c33362d9bc
Lee Jiwon <107186291+dlwldnjs1009@users.noreply.github.com>
2025-08-12 21:50:48 +0900

    Time: 2 ms (98.96%), Space: 45 MB (47.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        // nums for문으로 돌면서 더했을 때 target되면 인덱스 반환
-        for (int i = 0; i < nums.length;  i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans[0] = i;
-                    ans[1] = j;
-                }
-                
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(target - nums[i]) && i != numsMap.get(target - nums[i])) {
+                return new int[]{i, numsMap.get(target - nums[i])};
             }
+
+            numsMap.put(nums[i], i);
         }
-        return ans;
+
+        return null;
     }
 }
